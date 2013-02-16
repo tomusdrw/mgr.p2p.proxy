974e912b5e7dcefd35a83bc79762244b8addbb10
Tomusdrw <tomusdrw@gmail.com>
2013-02-16 19:04:35 +0100

    Regex replacement of images.

--- main.py
+++ main.py
@@ -5,10 +5,12 @@
 from proxy import ProxyFactory
 from twisted.internet import reactor
 from twisted.python import log
+import logging
 import sys
 
 
 if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     log.startLogging(open('logs/twisted.logs', 'a+'))
     
     reactor.listenTCP(8080, ProxyFactory.ProxyFactory())
--- proxy/ProxyRequest.py
+++ proxy/ProxyRequest.py
@@ -1,17 +1,89 @@
 '''
 @author: tomusdrw
 '''
-from twisted.web import proxy
 from proxy.TunnelProtocolFactory import TunnelProtocolFactory
+from twisted.web import http
+from twisted.internet import reactor
+from twisted.web.proxy import ProxyClientFactory
+import urlparse
+import re
+import logging
 
-class ProxyRequest(proxy.ProxyRequest):
+
+log = logging.getLogger(__name__)
+class ProxyRequest(http.Request):
+    """
+    Used by Proxy to implement a simple web proxy.
+
+    @ivar reactor: the reactor used to create connections.
+    @type reactor: object providing L{twisted.internet.interfaces.IReactorTCP}
+    """
+
+    protocols = {'http': ProxyClientFactory}
+    ports = {'http': 80}
+
+    def __init__(self, channel, queued, reactor2=reactor):
+        http.Request.__init__(self, channel, queued)
+        self.reactor = reactor2
+        
     def process(self):
         """ Support https proxying """
         if self.method == 'CONNECT':
             self.processConnect()
         else:
             print "Address: " + self.uri
-            return proxy.ProxyRequest.process(self)
+            self.processHttp()
+
+
+    def extractHostAndPort(self, parsed, protocol):
+        host = parsed[1]
+        port = self.ports[protocol]
+        if ':' in host:
+            host, port = host.split(':')
+            port = int(port)
+        return host, port
+
+
+    def extractQuery(self, parsed):
+        rest = urlparse.urlunparse(('', '') + parsed[2:])
+        if not rest:
+            rest = rest + '/'
+        return rest
+
+
+    def replyWithImageFromDisk(self):
+        f = open('foto_4.jpg', 'r')
+        imgContent = f.read()
+        f.close()
+        self.setResponseCode(200, "Found file on disk")
+        self.responseHeaders.addRawHeader("content-type", "image/jpeg")
+        self.write(imgContent)
+        self.finish()
+    
+    
+    def processHttp(self):
+        parsed = urlparse.urlparse(self.uri)
+        protocol = parsed[0]
+        host, port = self.extractHostAndPort(parsed, protocol)
+        rest = self.extractQuery(parsed)
+        
+        class_ = self.protocols[protocol]
+        headers = self.getAllHeaders().copy()
+        
+        if 'host' not in headers:
+            headers['host'] = host
+            
+        if re.match(r'.+\.png$', rest):
+            self.replyWithImageFromDisk()
+            log.warning('Replying with image from disk')
+        else:
+            log.info('Performing request for ' + self.uri)
+            self.content.seek(0, 0)
+            s = self.content.read()
+            clientFactory = class_(self.method, rest, self.clientproto, headers,
+                                   s, self)
+            self.reactor.connectTCP(host, port, clientFactory)
+
     def processConnect(self):
         try:
             host, portStr = self.uri.split(':', 1)
