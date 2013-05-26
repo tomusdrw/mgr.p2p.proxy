9fe5f1a0f8e9edae6c0406912f144a379ff77c0d
Tomusdrw <tomusdrw@gmail.com>
2013-05-26 12:50:23 +0200

    Added runner for nomem simulations.

---
+++ run_nomem_simulations.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ALGOS=(lfu lru fifo)
+SIZES=(128 256 512)
+
+for p2pAlgo in ${ALGOS[*]}
+do
+  for size in ${SIZES[*]}
+  do
+    echo "Running simulation for P2P ${p2pAlgo}(${size})"
+    time ./simulator_main.py --no-mem --p2p-algo $p2pAlgo --p2p-size $size > logs/run_no_mem_${p2pAlgo}_${size}.log
+  done
+done
