6be223746310e1a0e05e02c46ca060b352b20da3
Tomusdrw <tomusdrw@gmail.com>
2013-06-12 09:58:38 +0200

    Description of Kademlia and others part of document.

--- run_nomem_simulations.sh
+++ run_nomem_simulations.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-#NET=(kademlia chord)
-NET=(kademlia)
+NET=(kademlia kademlia kademlia kademlia)
+#NET=(kademlia)
 ALGOS=(lfu lru fifo)
-SIZES=(256 512 1024 2048)
+SIZES=(256 512 1024 2048 4096)
 
 # Increase UDP buffer size
-#sudo sysctl -w net.core.rmem_max=26214400
-#sudo sysctl -w net.core.rmem_default=26214400
+sudo sysctl -w net.core.rmem_max=26214400
+sudo sysctl -w net.core.rmem_default=26214400
 
 # This is (probably) not needed if no errors is reported in netstat -s
 #sudo sysctl -w net.core.wmem_max=5242880
 #sudo sysctl -w net.core.wmem_default=5242880
 
+I=0
 for network in ${NET[*]}
 do
   for p2pAlgo in ${ALGOS[*]}
@@ -23,4 +24,7 @@ do
       time ./simulator_main.py --no-mem --p2p-algo $p2pAlgo --p2p-size $size  --p2p-network $network > logs/run_no_mem_${network}_${p2pAlgo}_${size}.log
     done
   done
+  mv logs logs_$I
+  mkdir logs
+  I=$I+1
 done
